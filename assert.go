6d1273dffdf00a88c68ff171f57b0eddea021e26
Byron Ruth <b@devel.io>
2013-08-21 17:15:46 -0400

    Move default return to end of function for Go 1.0 compat

--- assert.go
+++ assert.go
@@ -71,9 +71,8 @@ func notContains(t *testing.T, unexpected, got string, callDepth int, messages .
 func isEqual(expected, got interface{}) bool {
     if expected == nil {
         return isNil(got)
-    } else {
-        return reflect.DeepEqual(expected, got)
     }
+    return reflect.DeepEqual(expected, got)
 }
 
 func isNil(got interface{}) bool {
@@ -84,9 +83,8 @@ func isNil(got interface{}) bool {
     switch value.Kind() {
     case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
         return value.IsNil()
-    default:
-        return false
     }
+    return false
 }
 
 // -- Public API
